7a9467986742a2d7ca12e1371210be5d46c9f414
Daniel Corcoran <danielc.92@outlook.com>
2021-02-26 21:13:17 +1100

    Created card component

---
+++ components/Card/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <h1 class="card__heading">{{ title }}</h1>
+    <p class="card__info">{{ info }}</p>
+    <p class="card__author">Author: {{ author }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    info: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style scoped lang="scss">
+/* stored in assets/styles/card.scss */
+</style>
