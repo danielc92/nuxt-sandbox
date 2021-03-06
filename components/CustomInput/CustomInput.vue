69df3e1f8187ec4b3164ca1318ab925e775caec5
Daniel Corcoran <danielc.92@outlook.com>
2021-03-06 21:38:39 +1100

    custom inputs

---
+++ components/CustomInput/CustomInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <label :for="$attrs.id">{{ label }}</label>
+    <input
+      type="text"
+      v-bind="$attrs"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    value: String,
+    label: String,
+  },
+})
+</script>
---
+++ pages/custom-inputs.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <custom-input v-model="firstName" :label="label1" />
+    <custom-input v-model="lastName" :label="label2" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import CustomInput from '~/components/CustomInput/CustomInput.vue'
+
+export default Vue.extend({
+  components: { CustomInput },
+  data() {
+    return {
+      label1: 'First name',
+      label2: 'Last name',
+      firstName: 'John',
+      lastName: 'Doe',
+    }
+  },
+})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
