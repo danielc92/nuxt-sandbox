9da37fa4acf868bbb8f38678e153ddd45b35c1f5
Daniel Corcoran <danielc.92@outlook.com>
2021-03-06 21:07:30 +1100

    Custom button component

---
+++ components/CustomButton/CustomButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ paragraph }}</p>
+    <button v-bind="$attrs" @click="emitClick">Click me</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      default: 'default title',
+    },
+    paragraph: {
+      type: String,
+      default: 'default paragraph',
+    },
+  },
+  methods: {
+    emitClick(event: any) {
+      this.$emit('customclick', event)
+    },
+  },
+})
+</script>
