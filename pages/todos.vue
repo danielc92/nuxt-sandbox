c7d15a6dea762af534215074a790d140b5ad78da
Daniel Corcoran <danielc.92@outlook.com>
2021-03-11 19:15:36 +1100

    async fetch example

---
+++ pages/todos.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <h1>Todos</h1>
+    <p>Mock todos from jsonplaceholder</p>
+    <div
+      v-for="t in todos"
+      :key="t.id"
+      class="todo"
+      :class="{ 'todo--completed': t.completed }"
+    >
+      <h3>{{ t.id }} - {{ t.title }}</h3>
+      <p>By user: {{ t.userId }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      todos: [],
+    }
+  },
+  async fetch() {
+    this.todos = await fetch(
+      'https://jsonplaceholder.typicode.com/todos'
+    ).then((response) => response.json())
+  },
+})
+</script>
+
+<style lang="scss">
+.container {
+  max-width: 760px;
+  margin: 0 auto;
+}
+
+.todo {
+  color: white;
+  background: rgb(202, 53, 53);
+  border: 2px solid #f2f2f2;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  border-radius: 4px;
+  &--completed {
+    background: rgb(47, 172, 47);
+  }
+}
+</style>
