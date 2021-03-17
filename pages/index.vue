6ca8955ed228e8d9ff08a56c15c4350921627ba3
Daniel Corcoran <danielc.92@outlook.com>
2021-03-17 18:22:13 +1100

    added page for props

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,9 @@
         <nuxt-link to="/conditionals">Conditionals</nuxt-link>
       </li>
       <li>
+        <nuxt-link to="/props">Props</nuxt-link>
+      </li>
+      <li>
         <nuxt-link to="/custom-inputs">Custom events with inputs</nuxt-link>
       </li>
       <li>
---
+++ pages/props.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Passing and validating component props</h1>
+    <card
+      title="Card title"
+      info="Et aliqua dolore consequat esse sint proident nulla veniam exercitation consectetur."
+      author="Lorem Ipsum"
+    ></card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Card from '~/components/Card/Card.vue'
+
+export default Vue.extend({ components: { Card } })
+</script>
