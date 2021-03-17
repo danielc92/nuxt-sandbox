ad84b0b6cf9c96b5b394a09ac2cd19ad9e877c16
Daniel Corcoran <danielc.92@outlook.com>
2021-03-17 18:18:19 +1100

    reorganized menu

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,10 @@
     <h1 class="home__title">Dan's Nuxt App</h1>
     <p>A sandbox environment to learn Nuxtjs fundamentals and module system.</p>
     <p>Pages are listed below, each one explores particular concept(s).</p>
+    <h3>Vue/Nuxtjs fundamentals</h3>
     <ul>
       <li>
         <nuxt-link to="/conditionals">Conditionals</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/charts">Charts</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/toasts">Toasts</nuxt-link>
       </li>
       <li>
         <nuxt-link to="/custom-inputs">Custom events with inputs</nuxt-link>
@@ -26,11 +21,22 @@
         <nuxt-link to="/todos">Rendering RestAPI data</nuxt-link>
       </li>
       <li>
-        <nuxt-link to="/apollo">Rendering GraphQL data</nuxt-link>
-      </li>
-      <li>
         <nuxt-link to="/images">Images</nuxt-link>
       </li>
+    </ul>
+    <h3>Modules</h3>
+    <ul>
+      <li>
+        <nuxt-link to="/charts">Charts</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/toasts">Toasts</nuxt-link>
+      </li>
+
+      <li>
+        <nuxt-link to="/apollo">Rendering GraphQL data</nuxt-link>
+      </li>
+
       <li>
         <nuxt-link to="/style-resources">Style resources</nuxt-link>
       </li>
